cbea51a6b0a6cfcd844873bf7156f9945f4ad6c1
nepp-plus <nepp.plus@gmail.com>
2021-11-21 11:50:29 +0900

    메인화면 새로 추가

---
+++ app/src/main/java/com/neppplus/keepthetime_20211121/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.neppplus.keepthetime_20211121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_20211121.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
