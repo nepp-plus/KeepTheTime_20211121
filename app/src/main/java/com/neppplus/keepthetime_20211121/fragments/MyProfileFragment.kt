35dbb7db99b272be899f69fd664603aecf02b77d
nepp-plus <nepp.plus@gmail.com>
2021-11-21 15:18:05 +0900

    BaseFragment :  모든 프래그먼트가 공통적으로 갖는 기능 / 변수를 모아두는 클래스

--- app/src/main/java/com/neppplus/keepthetime_20211121/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/fragments/MyProfileFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.keepthetime_20211121.R
 
-class MyProfileFragment : Fragment() {
+class MyProfileFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +19,21 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
 
-        
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+//        내 정보를 서버에서 받아오자. -> 이미지 반영 / 닉네임 반영
+
+//        1. 프래그먼트에서 retrofit 어떻게 활용?
+
+//        2. dataBinding ->  프래그먼트에서는 어떻게 데이터바인딩?
 
     }
 
