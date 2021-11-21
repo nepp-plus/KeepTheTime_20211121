98770204342716796b956b5c5b3d0ade9f1ea66c
nepp-plus <nepp.plus@gmail.com>
2021-11-21 14:56:05 +0900

    탭레이아웃 / 뷰페이저 / 프래그먼트2개 생성

---
+++ app/src/main/java/com/neppplus/keepthetime_20211121/fragments/MyProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.keepthetime_20211121.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_20211121.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        
+
+    }
+
+
+}
---
+++ app/src/main/java/com/neppplus/keepthetime_20211121/fragments/ScheduleListFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.keepthetime_20211121.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_20211121.R
+
+class ScheduleListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_schedule_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+
+
+}
