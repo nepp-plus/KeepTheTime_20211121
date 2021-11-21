77b00326e4a0049e340dfe4a69fe3e34e414ba3d
nepp-plus <nepp.plus@gmail.com>
2021-11-21 11:16:55 +0900

    실제 로그인 기능 수행  - 응답 : BasicResponse 형태로 돌아옴.  - body를 변수에 담아서 활용 -> 하위 변수에 파싱까지 전부 되어있었다.

--- app/src/main/java/com/neppplus/keepthetime_20211121/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/MainActivity.kt
@@ -2,10 +2,16 @@ package com.neppplus.keepthetime_20211121
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_20211121.api.ServerAPI
 import com.neppplus.keepthetime_20211121.api.ServerAPIService
 import com.neppplus.keepthetime_20211121.databinding.ActivityMainBinding
+import com.neppplus.keepthetime_20211121.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -28,6 +34,28 @@ class MainActivity : BaseActivity() {
 
 
 //            2. 서버에 로그인 API 호출 -> Retrofit
+
+            apiService.postRequestLogin(inputEmail, inputPassword).enqueue( object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+//                    최종 성공 / 실패 여부에 따라 별도 코딩
+                    if (response.isSuccessful) {
+                        val basicResponse =  response.body()!!
+                        Log.d("로그인성공",  basicResponse.message)
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                    Toast.makeText(mContext, "서버 연결에 실패했습니다.", Toast.LENGTH_SHORT).show()
+
+                }
+
+            } )
+
 
         }
 
--- app/src/main/java/com/neppplus/keepthetime_20211121/api/ServerAPIService.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/api/ServerAPIService.kt
@@ -1,17 +1,22 @@
 package com.neppplus.keepthetime_20211121.api
 
+import com.neppplus.keepthetime_20211121.datas.BasicResponse
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ServerAPIService {
 
 //    기능별 주소 (endpoint) / 메쏘드 (POST) / 파라미터 명시.
 
+//    POST / PUT / PATCH - FormData (제 서버 : FormUrlEncoded 처리 필요)
+
+    @FormUrlEncoded
     @POST("/user")
     fun postRequestLogin(
         @Field("email")  email: String,
         @Field("password")  pw: String
-    )
+    ) : Call<BasicResponse>
 
 }
