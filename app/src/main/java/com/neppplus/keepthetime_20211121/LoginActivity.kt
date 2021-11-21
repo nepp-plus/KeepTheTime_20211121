c963c5a40aa035b1db60161384966933a9adb593
nepp-plus <nepp.plus@gmail.com>
2021-11-21 11:48:43 +0900

    메인화면 이름변경 -> LoginActivity

--- app/src/main/java/com/neppplus/keepthetime_20211121/LoginActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/LoginActivity.kt
@@ -1,26 +1,23 @@
 package com.neppplus.keepthetime_20211121
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.neppplus.keepthetime_20211121.api.ServerAPI
-import com.neppplus.keepthetime_20211121.api.ServerAPIService
-import com.neppplus.keepthetime_20211121.databinding.ActivityMainBinding
+import com.neppplus.keepthetime_20211121.databinding.ActivityLoginBinding
 import com.neppplus.keepthetime_20211121.datas.BasicResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
     }
