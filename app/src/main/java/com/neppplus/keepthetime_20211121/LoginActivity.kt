7b101c498585086b97b63930b798e9f9b897cbee
nepp-plus <nepp.plus@gmail.com>
2021-11-21 14:50:01 +0900

    페북 로그인 실행 / 성공시 처리 코드 작성중  - 페북서버에 접근가능한 accessToken (REMOVED)  - 내 정보 (newMeRequest) 수신 -> 우리 앱서버에 전달

--- app/src/main/java/com/neppplus/keepthetime_20211121/LoginActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/LoginActivity.kt
@@ -7,7 +7,9 @@ import android.util.Base64
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.facebook.CallbackManager
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.neppplus.keepthetime_20211121.databinding.ActivityLoginBinding
 import com.neppplus.keepthetime_20211121.datas.BasicResponse
 import com.neppplus.keepthetime_20211121.utils.ContextUtil
@@ -16,6 +18,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.security.MessageDigest
+import java.util.*
 
 class LoginActivity : BaseActivity() {
 
@@ -35,7 +38,42 @@ class LoginActivity : BaseActivity() {
 
         binding.btnFacebookLogin.setOnClickListener {
 
-//            소셜 로그인 로직 체험험
+//            소셜 로그인 로직 체험
+
+//            페북로그인에 성공하게 되면 할일도 미리 설정.
+
+            LoginManager.getInstance().registerCallback( callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+
+//                    페북 로그인 성공 -> 그 결과로 토큰 받아오기 성공.
+
+                    Log.d("로그인성공", AccessToken.getCurrentAccessToken().toString())
+
+//                    이 토큰을 이용해서 -> 페북에서 사용자 정보도 받아오자. => 페북 클래스 : GraphRequest
+
+                    GraphRequest.newMeRequest(result?.accessToken, object : GraphRequest.GraphJSONObjectCallback {
+
+                        override fun onCompleted(jsonObj: JSONObject?, response: GraphResponse?) {
+
+                            Log.d("내정보",  jsonObj.toString())
+                            Log.d("내정보응답",  response.toString())
+                        }
+
+                    })
+
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            } )
+
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList( "public_profile", "email" ))
 
         }
 
