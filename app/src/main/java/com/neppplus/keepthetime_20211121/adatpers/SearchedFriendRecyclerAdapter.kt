c2ed9981bd0ab16fabc486c6a27d756e4635f9f4
nepp-plus <nepp.plus@gmail.com>
2021-11-28 14:49:22 +0900

    검색 목록 실제 데이터 반영

--- app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/SearchedFriendRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/SearchedFriendRecyclerAdapter.kt
@@ -4,13 +4,28 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.neppplus.keepthetime_20211121.R
 import com.neppplus.keepthetime_20211121.datas.UserData
 
 class SearchedFriendRecyclerAdapter(val mContext: Context, val mList: List<UserData>) : RecyclerView.Adapter<SearchedFriendRecyclerAdapter.SearchedUserViewHolder>() {
 
     inner class SearchedUserViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
+        val btnAddFriend = row.findViewById<Button>(R.id.btnAddFriend)
+
+        fun bind( data: UserData ) {
+
+            txtNickname.text = data.nickname
+            Glide.with(mContext).load(data.profileImageURL).into(imgProfile)
+
+        }
 
     }
 
@@ -20,6 +35,8 @@ class SearchedFriendRecyclerAdapter(val mContext: Context, val mList: List<UserD
     }
 
     override fun onBindViewHolder(holder: SearchedUserViewHolder, position: Int) {
+
+        holder.bind( mList[position] )
 
     }
 
