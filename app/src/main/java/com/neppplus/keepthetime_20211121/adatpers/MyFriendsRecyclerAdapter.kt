9f7c2f2602c5a020cda492ec4e1677c32a1a5b45
nepp-plus <nepp.plus@gmail.com>
2021-11-28 13:38:53 +0900

    리싸이클러 어댑터 - 실제 데이터 반영   - inner class의 bind 함수 추가 / 활용

--- app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/MyFriendsRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/MyFriendsRecyclerAdapter.kt
@@ -4,13 +4,26 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.neppplus.keepthetime_20211121.R
 import com.neppplus.keepthetime_20211121.datas.UserData
 
 class MyFriendsRecyclerAdapter(val mContext: Context, val mList: List<UserData>) : RecyclerView.Adapter<MyFriendsRecyclerAdapter.MyFriendViewHolder>() {
 
     inner class MyFriendViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
+
+        fun bind( data: UserData ) {
+
+            txtNickname.text = data.nickname
+            Glide.with(mContext).load(data.profileImageURL).into(imgProfile)
+
+        }
 
     }
 
@@ -22,6 +35,8 @@ class MyFriendsRecyclerAdapter(val mContext: Context, val mList: List<UserData>)
     }
 
     override fun onBindViewHolder(holder: MyFriendViewHolder, position: Int) {
+
+        holder.bind( mList[position] )
 
     }
 
