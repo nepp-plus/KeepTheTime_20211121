4795061845a41b2222582332b84ed79ec3c7d748
nepp-plus <nepp.plus@gmail.com>
2021-11-28 13:45:09 +0900

    스플래쉬 화면 추가 -> 시작화면으로 변경

---
+++ app/src/main/java/com/neppplus/keepthetime_20211121/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.keepthetime_20211121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
