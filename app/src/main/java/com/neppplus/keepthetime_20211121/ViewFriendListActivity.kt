52f5cb17f62a2a19a4ce7355ba1e1485af823533
nepp-plus <nepp.plus@gmail.com>
2021-11-28 12:41:45 +0900

    내 친구 목록 -> ArrayList에 추가

--- app/src/main/java/com/neppplus/keepthetime_20211121/ViewFriendListActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/ViewFriendListActivity.kt
@@ -16,6 +16,7 @@ class ViewFriendListActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewFriendListBinding
 
+    val mMyFriendsList = ArrayList<UserData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +31,29 @@ class ViewFriendListActivity : BaseActivity() {
 
     override fun setValues() {
 
+        getMyFriendsFromServer()
+    }
+
+    fun getMyFriendsFromServer() {
+
+        apiService.getRequestMyFriends("my").enqueue( object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    val br = response.body()!!
+
+                    mMyFriendsList.addAll( br.data.friends )
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        } )
 
     }
 
