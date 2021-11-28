0ed7392061ff62cc3897523924a9617a3aa951f6
nepp-plus <nepp.plus@gmail.com>
2021-11-28 12:20:24 +0900

    리스트뷰 -> 리싸이클러뷰로 작업 예정 (화면 비우기)

--- app/src/main/java/com/neppplus/keepthetime_20211121/ViewFriendListActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/ViewFriendListActivity.kt
@@ -16,9 +16,6 @@ class ViewFriendListActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewFriendListBinding
 
-    val mMyFriendList = ArrayList<UserData>()
-
-    lateinit var mFriendAdapter: MyFriendsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,34 +30,6 @@ class ViewFriendListActivity : BaseActivity() {
 
     override fun setValues() {
 
-        getMyFriendsFromServer()
-
-        mFriendAdapter = MyFriendsAdapter(mContext, R.layout.my_friend_list_item, mMyFriendList)
-        binding.myFriendListView.adapter = mFriendAdapter
-
-    }
-
-    fun getMyFriendsFromServer() {
-
-        apiService.getRequestMyFriends("my").enqueue(object : Callback<BasicResponse> {
-            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
-
-                if (response.isSuccessful) {
-
-                    val br = response.body()!!
-
-                    mMyFriendList.addAll( br.data.friends )
-
-                    mFriendAdapter.notifyDataSetChanged()
-                }
-
-            }
-
-            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-
-            }
-
-        })
 
     }
 
