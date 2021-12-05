4efc4913dd06667fbce8f5189abbbb3edf1ab863
nepp-plus <nepp.plus@gmail.com>
2021-12-05 12:36:36 +0900

    네이버지도 화면에 띄우기   - fragment를 xml에 직접 올리기

--- app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
@@ -172,10 +172,8 @@ class EditAppointmentActivity : BaseActivity() {
             val finalDateTimeStr =  serverFormat.format( mSelectedDateTime.time )
 
             val inputPlace = binding.edtPlace.text.toString()
-            val inputLat = binding.edtLatitude.text.toString().toDouble()
-            val inputLng = binding.edtLongitude.text.toString().toDouble()
 
-            apiService.postRequestAppointment(inputTitle, finalDateTimeStr, inputPlace, inputLat, inputLng).enqueue( object : Callback<BasicResponse> {
+            apiService.postRequestAppointment(inputTitle, finalDateTimeStr, inputPlace, 37.123, 127.123).enqueue( object : Callback<BasicResponse> {
                 override fun onResponse(
                     call: Call<BasicResponse>,
                     response: Response<BasicResponse>
