e009ba6f05f28487d9bab1cd42fa0b1ac0470f8d
nepp-plus <nepp.plus@gmail.com>
2021-12-05 15:49:13 +0900

    네이버 지도 화면에 띄우기 + 생명주기 함수 연결

--- app/src/main/java/com/neppplus/keepthetime_20211121/ViewPlaceMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/ViewPlaceMapActivity.kt
@@ -12,6 +12,7 @@ class ViewPlaceMapActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_place_map)
+        binding.naverMapView.onCreate(savedInstanceState)
         setupEvents()
         setValues()
     }
@@ -22,5 +23,51 @@ class ViewPlaceMapActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        0. 프로젝트에 네이버지도 설치 (완료)
+
+//        1. 화면 (xml) 에 네이버 맵 띄워주기
+
+
+//        2. 네이버 맵 객체를 실제로 얻어내기 -> getMapAsync
+
+//        3. 카메라 이동 / 마커 추가  (받아온 스케쥴의 위도/경도 이용)
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.naverMapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.naverMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.naverMapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.naverMapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.naverMapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.naverMapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.naverMapView.onLowMemory()
+    }
+
 }
--- app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/ScheduleRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/ScheduleRecyclerAdapter.kt
@@ -30,6 +30,9 @@ class ScheduleRecyclerAdapter(val mContext: Context, val mList: List<ScheduleDat
             imgMap.setOnClickListener {
 
                 val myIntent = Intent(mContext, ViewPlaceMapActivity::class.java)
+
+//                어떤 약속을 보러 가는지 데이터 첨부
+
                 mContext.startActivity(myIntent)
 
             }
