4138f2d8ec7e4cf28f2c1608946c055f861edc9c
nepp-plus <nepp.plus@gmail.com>
2021-12-05 11:40:48 +0900

    실제 시간 선택 -> 저장 / 문구 반영  => SimpleDateFormat 활용

--- app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
@@ -42,7 +42,20 @@ class EditAppointmentActivity : BaseActivity() {
             val timeSetListener =  object : TimePickerDialog.OnTimeSetListener {
                 override fun onTimeSet(p0: TimePicker?, hourOfDay: Int, minute: Int) {
 
-                    Log.d("선택된시간", "${hourOfDay}시, ${minute}분")
+//                    Log.d("선택된시간", "${hourOfDay}시, ${minute}분")
+
+//                    선택한 시간도 실제로 저장
+                    mSelectedDateTime.set( Calendar.HOUR_OF_DAY, hourOfDay  )
+                    mSelectedDateTime.set( Calendar.MINUTE, minute  )
+
+//                    선택한 시간을 -> "오후 4:05"   형태로 txtTime에 표시
+//                    SimpleDateFormat 활용.
+
+                    val timeFormat = SimpleDateFormat("a h:mm")
+                    val timeStr = timeFormat.format( mSelectedDateTime.time )
+
+                    binding.txtTime.text = timeStr
+
 
                 }
             }
