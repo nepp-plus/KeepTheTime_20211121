d4f9bddc7890e2366ad5a6f8edfcc45359942880
nepp-plus <nepp.plus@gmail.com>
2021-12-05 11:09:13 +0900

    SimpleDateFormat 활용  - Calendar에 저장된 날짜를 -> String으로 변환해서 텍스트뷰에 출력

--- app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
@@ -11,6 +11,7 @@ import com.neppplus.keepthetime_20211121.datas.BasicResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
 import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
@@ -52,6 +53,15 @@ class EditAppointmentActivity : BaseActivity() {
 //                    년/월/일을 한번에 저장하는 set 함수 활용
                     mSelectedDateTime.set(year, month, dayOfMonth)
 
+//                    txtDate 의 문구를 -> 2021-08-05  와 같은 양식으로 가공해서 텍스트 세팅.
+
+//                    Calendar를 다룰 양식만 미리 지정.
+                    val dateFormat = SimpleDateFormat( "yyyy-MM-dd" )
+
+//                    Calendar => String 변환.
+                    val dateStr = dateFormat.format( mSelectedDateTime.time )
+
+                    binding.txtDate.text = dateStr
 
                 }
 
