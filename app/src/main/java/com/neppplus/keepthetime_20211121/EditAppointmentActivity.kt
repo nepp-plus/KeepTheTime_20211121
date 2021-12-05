db8ef8102c2e99822f8fcb4f323da3954278bb55
nepp-plus <nepp.plus@gmail.com>
2021-12-05 12:43:58 +0900

    네이버 지도 생명주기 맞춰주기  - 네이버 공식문서의 요청사항

--- app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/EditAppointmentActivity.kt
@@ -194,4 +194,40 @@ class EditAppointmentActivity : BaseActivity() {
     override fun setValues() {
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.naverMapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.naverMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.naverMapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.naverMapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.naverMapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.naverMapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.naverMapView.onLowMemory()
+    }
+
 }
