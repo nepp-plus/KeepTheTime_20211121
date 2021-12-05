b45cfde75707776d2a428963f41414c7c683541e
nepp-plus <nepp.plus@gmail.com>
2021-12-05 14:50:43 +0900

    일정목록 - 기본적인 약속 데이터 표시

--- app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/ScheduleRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/ScheduleRecyclerAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.neppplus.keepthetime_20211121.R
 import com.neppplus.keepthetime_20211121.datas.ScheduleData
@@ -11,6 +13,18 @@ import com.neppplus.keepthetime_20211121.datas.ScheduleData
 class ScheduleRecyclerAdapter(val mContext: Context, val mList: List<ScheduleData>) : RecyclerView.Adapter<ScheduleRecyclerAdapter.ScheduleViewHolder>() {
 
     inner class ScheduleViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+        val txtAppointmentTitle = row.findViewById<TextView>(R.id.txtAppointmentTitle)
+        val txtAppointmentPlace = row.findViewById<TextView>(R.id.txtAppointmentPlace)
+        val txtDateTime = row.findViewById<TextView>(R.id.txtDateTime)
+        val imgMap = row.findViewById<ImageView>(R.id.imgMap)
+
+        fun bind( data: ScheduleData ) {
+
+            txtAppointmentTitle.text =  data.title
+            txtAppointmentPlace.text = data.place
+
+        }
 
     }
 
@@ -22,6 +36,8 @@ class ScheduleRecyclerAdapter(val mContext: Context, val mList: List<ScheduleDat
     }
 
     override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
+
+        holder.bind( mList[position] )
 
     }
 
