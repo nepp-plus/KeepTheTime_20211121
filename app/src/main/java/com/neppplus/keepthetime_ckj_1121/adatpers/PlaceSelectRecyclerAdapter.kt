47e4ea49d45bfe93aa26db4af91dd4dd32fe8378
nepp-plus <nepp.plus@gmail.com>
2021-12-12 15:36:43 +0900

    리싸이클러뷰 한칸 (ViewHolder-row) 이 눌리면 할일 지정   - onItemClickListener에 할일이 적혀있는지? 적혀있다면 그 내용대로 실행.

--- app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
@@ -14,18 +14,34 @@ class PlaceSelectRecyclerAdapter(val mContext:Context, val mList: List<PlaceData
 //    장소 선택 리싸이클러뷰의 클릭이벤트 =>  액티비티 or 프래그먼트 등으로 할일을 넘겨주자. (interface 활용)
 
     interface OnItemClickListener {
-        fun onItemClick( position: Int )
+        fun onItemClick( data: PlaceData )
     }
 
-    inner class  PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+//    인터페이스를 변수로 설정.
+
+//    null : 아이템 클릭시 할일이 없다.
+//    null 아니다 : 할일이 지정된 상태다.
+    var onItemClickListener : OnItemClickListener? = null
+
+
+    inner class  PlaceViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
 
 //        멤버변수 - list_item 내부의 UI
         val txtPlaceName = row.findViewById<TextView>(R.id.txtPlaceName)
 
 //        함수 - 실제 데이터 연동 bind
+
         fun bind( data: PlaceData ) {
 
             txtPlaceName.text =  data.placeName
+
+//            한 줄이 눌리면 할일
+            row.setOnClickListener {
+
+//                아이템클릭리스너에 적혀있는 코드 실행.
+                onItemClickListener?.onItemClick( data )
+
+            }
 
         }
 
