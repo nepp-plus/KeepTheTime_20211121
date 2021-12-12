c86871000de0d7fc06d1e6c4511144f308c926e6
nepp-plus <nepp.plus@gmail.com>
2021-12-12 15:08:48 +0900

    실제 장소 목록 이름 표시

--- app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.neppplus.keepthetime_ckj_1121.R
 import com.neppplus.keepthetime_ckj_1121.datas.PlaceData
@@ -12,7 +13,15 @@ class PlaceSelectRecyclerAdapter(val mContext:Context, val mList: List<PlaceData
 
     inner class  PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row) {
 
+//        멤버변수 - list_item 내부의 UI
+        val txtPlaceName = row.findViewById<TextView>(R.id.txtPlaceName)
 
+//        함수 - 실제 데이터 연동 bind
+        fun bind( data: PlaceData ) {
+
+            txtPlaceName.text =  data.placeName
+
+        }
 
     }
 
@@ -24,6 +33,8 @@ class PlaceSelectRecyclerAdapter(val mContext:Context, val mList: List<PlaceData
     }
 
     override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+
+        holder.bind( mList[position] )
 
     }
 
