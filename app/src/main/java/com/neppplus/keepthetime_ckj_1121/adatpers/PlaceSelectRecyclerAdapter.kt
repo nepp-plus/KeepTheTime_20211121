d7fbf11e7f7f09ed851215b65160f359c9066ba2
nepp-plus <nepp.plus@gmail.com>
2021-12-12 15:02:05 +0900

    리싸이클러 어댑터 뼈대작업 마무리

--- app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
@@ -1,14 +1,32 @@
 package com.neppplus.keepthetime_ckj_1121.adatpers
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.keepthetime_ckj_1121.R
+import com.neppplus.keepthetime_ckj_1121.datas.PlaceData
 
-class PlaceSelectRecyclerAdapter : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+class PlaceSelectRecyclerAdapter(val mContext:Context, val mList: List<PlaceData>) : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
 
     inner class  PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row) {
 
 
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.place_list_item, parent, false)
+        return  PlaceViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
 
 }
