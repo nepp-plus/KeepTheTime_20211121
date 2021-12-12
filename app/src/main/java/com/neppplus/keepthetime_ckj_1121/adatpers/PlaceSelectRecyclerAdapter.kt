8f64060b27352812d6604d935b7a322bb96fc378
nepp-plus <nepp.plus@gmail.com>
2021-12-12 14:58:51 +0900

    얼럿 내부에서 사용할 리싸이클러 어댑터 생성중

---
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/PlaceSelectRecyclerAdapter.kt
@@ -0,0 +1,14 @@
+package com.neppplus.keepthetime_ckj_1121.adatpers
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceSelectRecyclerAdapter : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+
+    inner class  PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+
+
+    }
+
+}
