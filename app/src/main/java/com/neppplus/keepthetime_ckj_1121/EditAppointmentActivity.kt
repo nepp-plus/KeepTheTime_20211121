51074d565b32616e54cecefeeefe1f73581f8cca
nepp-plus <nepp.plus@gmail.com>
2021-12-12 15:55:22 +0900

    별도 함수 추가 - 임시로 장소명을 반영

--- app/src/main/java/com/neppplus/keepthetime_ckj_1121/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/EditAppointmentActivity.kt
@@ -159,8 +159,19 @@ class EditAppointmentActivity : BaseActivity() {
                         placeAdapter.onItemClickListener = object : PlaceSelectRecyclerAdapter.OnItemClickListener {
                             override fun onItemClick(data: PlaceData) {
 
+//                                  임시 기능 : 토스트로 가게 이름 출력
+//                                Toast.makeText(mContext, data.placeName, Toast.LENGTH_SHORT).show()
 
-                                Toast.makeText(mContext, data.placeName, Toast.LENGTH_SHORT).show()
+//                                실제 기능 :  장소를 선택한곳으로 지정하기.
+
+                                setPlaceDataToNaverMap(data)
+
+//                                추가기능 : 열려있는 팝업창 닫기
+
+
+
+
+
 
                             }
 
@@ -542,5 +553,18 @@ class EditAppointmentActivity : BaseActivity() {
         super.onLowMemory()
         binding.naverMapView.onLowMemory()
     }
+
+
+//    장소를 선택하면 => 지도에 반영해주는 함수
+
+    fun setPlaceDataToNaverMap( placeData: PlaceData ) {
+
+//        약속장소 입력값 변경
+
+        binding.edtPlace.setText( placeData.placeName )
+
+
+    }
+
 
 }
