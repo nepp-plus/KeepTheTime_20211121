c86e37c35c8c51113ebe9ea75208cda24bc49fc5
nepp-plus <nepp.plus@gmail.com>
2021-12-19 12:21:51 +0900

    출발 장소 목록 어댑터 작업  => 리스트뷰와 완전히 동일한 수준 작업.

--- app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/StartingPointSpinnerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/adatpers/StartingPointSpinnerAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.keepthetime_ckj_1121.adatpers
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.keepthetime_ckj_1121.R
 import com.neppplus.keepthetime_ckj_1121.datas.PlaceData
 
 class StartingPointSpinnerAdapter(
@@ -10,6 +14,21 @@ class StartingPointSpinnerAdapter(
     val mList: List<PlaceData>
 ) : ArrayAdapter<PlaceData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var tempRow = convertView
+        if (tempRow == null) {
+
+//            돌려막기 할 만큼 충분한 row가 그려지지 않은 상태.
+//            mInflater 변수 대신, 바로 LayoutInflater.from 기능 활용
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.starting_point_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
 
 }
