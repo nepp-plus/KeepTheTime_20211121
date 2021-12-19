a9d823b9368ffaf1a23e9720096cb41c37589021
nepp-plus <nepp.plus@gmail.com>
2021-12-19 10:37:51 +0900

    네이버 지도 띄우고 / 지도 객체 받아오기  - 코드에서 동작 관련 기능 활용 가능

--- app/src/main/java/com/neppplus/keepthetime_ckj_1121/EditStartingPointActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_ckj_1121/EditStartingPointActivity.kt
@@ -12,9 +12,12 @@ class EditStartingPointActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_starting_point)
+        binding.mapView.onCreate(savedInstanceState)
         setupEvents()
         setValues()
     }
+
+
 
     override fun setupEvents() {
 
@@ -22,5 +25,50 @@ class EditStartingPointActivity : BaseActivity() {
 
     override fun setValues() {
 
+        binding.mapView.getMapAsync {
+
+            val naverMap = it
+
+
+
+        }
+
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
 }
